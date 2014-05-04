380cf9aa575ddae28e157751b3965f649e78b58e
Sergii Iarovyi <serge.zp.ua@gmail.com>
2014-05-04 16:13:07 +0300

    font icons

--- scripts/app.js
+++ scripts/app.js
@@ -25,16 +25,18 @@ $(function(){
 
 
 
-    $('header li a').on('click', function(){
+    $('#skills-icons-list').html($('#logo-slider ul').html());
+
+    $('.header li a').on('click', function(){
         $('#menu-toggle').trigger('click');
     });
 
     $('#menu-toggle').on('click', function(){
-        $('header ul').toggleClass('zero-height-xs');
+        $('.header ul').toggleClass('zero-height-xs');
     });
 
     $('body').on('activate.bs.scrollspy', function(e){
-        $('header').toggleClass('thin-header', $('a', e.target).attr('href') !== '#landing-page')
+        $('.header').toggleClass('thin-header', $('a', e.target).attr('href') !== '#landing-page')
     });
 
     console && console.log('%cHi, curious man!', 'color: orange; font-size: 4em; border: 2px solid #8CC63F');
